b685e9a405b4325e2c386d18ccc9979953d8f2b1
Che-Yi Kung <Che-Yikung@Che-Yide-MacBook-Pro.local>
2019-12-16 20:41:14 -0800

    basic layout

---
+++ frontend/nutrilion/src/containers/LandingPage/LandingPage.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const LandingPage = () => {
+    const [ info, setInfo ] = useState({
+        name: '',
+        phone: '',
+    })
+
+    return (
+        <div>
+            <input />
+            <input />
+        </div>
+    )
+}
+
+export default LandingPage;
